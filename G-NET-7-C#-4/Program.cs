f4a15a7442654a755ca8e2ddb6e0a57e08c5e5e3
Menna Allah <mennaallah738@gmail.com>
2026-04-10 20:44:56 +0200

    Question 01 :

--- G-NET-7-C#-4/Program.cs
+++ G-NET-7-C#-4/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace G_NET_7_C__4
 {
@@ -10,7 +14,19 @@ namespace G_NET_7_C__4
     {
         static void Main(string[] args)
         {
+            #region Question 01 :
+            //(a)Explain why this code is inefficient.Reference what happens in memory.
+           //In C#, strings are immutable. This means that every time the following line is executed:
+           //productList += "PROD-" + i + ",";
+            //a new string object is created in memory instead of modifying the existing one.
 
+            //The previous string becomes unused and is later cleaned up by the Garbage Collector.
+            //When this operation is repeated 5000 times, it results in:
+
+            //Creating thousands of temporary string objects
+            //Increased memory usage
+            //Slower performance due to repeated allocations
+            #endregion
         }
     }
 }
