5616f64eee43c3be3e345fbce1c161a767e6740d
Menna Allah <mennaallah738@gmail.com>
2026-04-10 20:40:23 +0200

    Add project files.

---
+++ G-NET-7-C#-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_7_C__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
