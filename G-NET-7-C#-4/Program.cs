76d5be7a6564b8d487c446961c247cb833797373
Menna Allah <mennaallah738@gmail.com>
2026-04-10 20:49:17 +0200

    Question 01 :

--- G-NET-7-C#-4/Program.cs
+++ G-NET-7-C#-4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Security.Policy;
@@ -16,8 +17,8 @@ namespace G_NET_7_C__4
         {
             #region Question 01 :
             //(a)Explain why this code is inefficient.Reference what happens in memory.
-           //In C#, strings are immutable. This means that every time the following line is executed:
-           //productList += "PROD-" + i + ",";
+            //In C#, strings are immutable. This means that every time the following line is executed:
+            //productList += "PROD-" + i + ",";
             //a new string object is created in memory instead of modifying the existing one.
 
             //The previous string becomes unused and is later cleaned up by the Garbage Collector.
@@ -26,6 +27,46 @@ namespace G_NET_7_C__4
             //Creating thousands of temporary string objects
             //Increased memory usage
             //Slower performance due to repeated allocations
+
+            //(b)Rewrite this code using StringBuilder to be more efficient.
+            //A more efficient approach is to use StringBuilder, which allows modifying the same object without creating new ones.
+
+            //using System.Text;
+            //StringBuilder productList = new StringBuilder();
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    productList.Append("PROD-" + i + ",");
+            //}
+            //string result = productList.ToString();
+            //Console.WriteLine(result);
+
+            //Advantages:
+            //Better performance
+            //Lower memory consumption
+            //No unnecessary object creation
+
+            //(c) Add timing code (using Stopwatch) to both versions and report the time difference.
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            //string productList = "";
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    productList += "PROD-" + i + ",";
+            //}
+            //sw.Stop();
+            //Console.WriteLine("String Time: " + sw.ElapsedMilliseconds + " ms");
+            //sw.Reset();
+            //sw.Start();
+            //StringBuilder sb = new StringBuilder();
+            //for (int i = 1; i <= 5000; i++)
+            //{
+            //    sb.Append("PROD-" + i + ",");
+            //}
+            //sw.Stop();
+            //Console.WriteLine("StringBuilder Time: " + sw.ElapsedMilliseconds + " ms");
+            #endregion
+
+            #region 
             #endregion
         }
     }
